d9431c6d19bd9cd6d04bfb9abf2057cecb061460
fbeiger <fbeiger971@gmail.com>
2016-02-27 16:18:08 +0100

    Change Adaptable

--- Digy Project/src/View/AdaptableViewLogIn.java
+++ Digy Project/src/View/AdaptableViewLogIn.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 //ViewLogIn doit hériter de Jframe et de Observable
 //On passe donc par le design pattern Adaptable
 public class AdaptableViewLogIn extends JFrame{
-	public ObservableViewLogIn viewObservable;
+	public ViewLogIn viewLog;
 	
 	public AdaptableViewLogIn(){
 		
--- Digy Project/src/View/ViewLogIn.java
+++ Digy Project/src/View/ViewLogIn.java
@@ -5,5 +5,5 @@ package View;
 //Quand l'utilisateur appuie sur le boutton 'Submit'
 //Appelle le controller avec les infos (user_id & password)
 public class ViewLogIn extends AdaptableViewLogIn{
-
+	
 }
