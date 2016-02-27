5fe3fc8b681c3176a7e87a59ce38ef3dbf4b0623
fbeiger <fbeiger971@gmail.com>
2016-02-27 16:09:07 +0100

    Add comments

--- Digy Project/src/View/AdaptableViewLogIn.java
+++ Digy Project/src/View/AdaptableViewLogIn.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 //ViewLogIn doit hériter de Jframe et de Observable
 //On passe donc par le design pattern Adaptable
 public class AdaptableViewLogIn extends JFrame{
+	public ObservableViewLogIn viewObservable;
+	
+	//Constructeur de la classe
+	//Doit créer la vue avec 2 texts fields, 2 labels et 1 button
 	public AdaptableViewLogIn(){
 		
 	}
