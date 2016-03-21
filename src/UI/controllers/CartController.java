d2f381faff832462bc0b96b6b6f7c1d11679b0a3
ArthurJamelot <arthur.jamelot@gmail.com>
2016-03-21 14:50:38 +0100

    Add the beginning of the cart, may change

---
+++ src/UI/controllers/CartController.java
@@ -0,0 +1,16 @@
+package UI.controllers;
+
+import app.facades.CartFacade;
+
+/**
+ * Created on 21/03/2016.
+ *
+ * @author Arthur
+ */
+public class CartController {
+    private CartFacade cartFacade = new CartFacade();
+
+    public CartController(RootController rootController){
+
+    }
+}
---
+++ src/app/facades/CartFacade.java
@@ -0,0 +1,12 @@
+package app.facades;
+
+import app.services.CartService;
+
+/**
+ * Created on 21/03/2016.
+ *
+ * @author Arthur
+ */
+public class CartFacade {
+    private CartService cartService;
+}
