718af43300a6564e797ccc8cbe7f6c8c0a75fe83
fbeiger <fbeiger971@gmail.com>
2016-03-27 19:49:49 +0200

    update menu
    Menu is according to the User Type

--- src/UI/layouts/MainLayout.java
+++ src/UI/layouts/MainLayout.java
@@ -20,6 +20,9 @@ import javax.swing.JPanel;
 import UI.core.Frame;
 import UI.core.LayoutInterface;
 import UI.core.RootController;
+import app.model.Admin;
+import app.model.Customer;
+import app.model.Seller;
 
 public class MainLayout extends RootController implements LayoutInterface {
 
@@ -51,7 +54,18 @@ public class MainLayout extends RootController implements LayoutInterface {
 		Image scaledImage = myPicture.getScaledInstance(140, 90, Image.SCALE_SMOOTH);
 		JLabel picLabel = new JLabel(new ImageIcon(scaledImage));
 		header.add(picLabel, BorderLayout.NORTH);
-		header.add(this.buildMenu(), BorderLayout.SOUTH);
+		
+		//Load the good menu according to the user.
+		if(getUser() instanceof Customer){
+			header.add(this.buildMenuCustomer(), BorderLayout.SOUTH);
+		}
+		if(getUser() instanceof Seller){
+			header.add(this.buildMenuSeller(), BorderLayout.SOUTH);
+		}
+		if(getUser() instanceof Admin){
+			header.add(this.buildMenuAdmin(), BorderLayout.SOUTH);
+		}
+		
 		header.add(new JLabel("Hi,  " + getUser().getUserID()), BorderLayout.CENTER);
 		
 		JPanel buttonsEdit = new JPanel();
@@ -77,7 +91,7 @@ public class MainLayout extends RootController implements LayoutInterface {
 		frame.center();
 	}
 	
-	public JPanel buildMenu() {
+	public JPanel buildMenuCustomer() {
 		JPanel menu = new JPanel(new FlowLayout());
 		MenuItem items[] = {
 				new MenuItem("Home", "home"),
@@ -87,6 +101,39 @@ public class MainLayout extends RootController implements LayoutInterface {
 				new MenuItem("Cart", "cart")
 		};	
 		for(MenuItem item: items) {
+			JButton button = new JButton(item.label);
+			button.addActionListener(new Navigate(this, item.action));
+			menu.add(button);
+		}
+		return menu;
+	}
+	
+	public JPanel buildMenuAdmin(){
+		JPanel menu = new JPanel(new FlowLayout());
+		MenuItem items[] ={
+				new MenuItem("Notifications", "notifications"),
+				new MenuItem("Manage Users", "mUsers"),
+				new MenuItem("Manage Activities", "mActivities"),
+				new MenuItem("Manage products", "mProducts")
+		};
+		for(MenuItem item: items){
+			JButton button = new JButton(item.label);
+			button.addActionListener(new Navigate(this, item.action));
+			menu.add(button);
+		}
+		return menu;
+	}
+	
+	public JPanel buildMenuSeller(){
+		JPanel menu = new JPanel(new FlowLayout());
+		MenuItem items[] = {
+				new MenuItem("Notifications", "notifications"),
+				new MenuItem("My account", "account"),
+				new MenuItem("My offers", "offers"),
+				new MenuItem("Products", "productList"),
+				new MenuItem("Quotes", "quotes")
+		};
+		for(MenuItem item: items){
 			JButton button = new JButton(item.label);
 			button.addActionListener(new Navigate(this, item.action));
 			menu.add(button);
@@ -113,7 +160,6 @@ public class MainLayout extends RootController implements LayoutInterface {
 			this.action = action;
 		}
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("test");
 			this.controller.goTo(action);
 		}
 	}
