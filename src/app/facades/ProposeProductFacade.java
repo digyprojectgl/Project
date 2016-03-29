c89ec31163d882a818ed236fc69fe757db9bc2d0
fbeiger <fbeiger971@gmail.com>
2016-03-29 19:36:56 +0200

    add link with propose product view

--- src/app/facades/ProposeProductFacade.java
+++ src/app/facades/ProposeProductFacade.java
@@ -4,19 +4,17 @@ import app.model.Product;
 import app.model.ProductCategory;
 import app.services.ProductProposedService;
 
+/**
+ * @author shui
+ */
 public class ProposeProductFacade {
 	private ProductProposedService proposeProductservice;
 	
 	public Product proposeproduct(String label, String description, ProductCategory category) throws Exception{
-		proposeProductservice = new ProductProposedService();
 		return proposeProductservice.proposeProduct(label, description, category);
 	}
 	
 	public void deleteproductproposed(String label, String description, ProductCategory category) throws Exception{
-		proposeProductservice = new ProductProposedService();
 		proposeProductservice.deleteProposition(label, label, category);
 	}
 }
-/**
- * @author shui
- */
